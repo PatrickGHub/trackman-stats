83af6f28fb81bd5f5d7cfdc24fe0935b77730542
PatrickGHub <patrick@pkelly.co>
2022-08-05 22:19:12 +0100

    Handle paginated sessions Further filter data

--- cloud/trackman/index.js
+++ cloud/trackman/index.js
@@ -9,12 +9,12 @@ const baseUrl = 'https://api.trackmanrange.com/api'
 
 axios.defaults.headers.common['Authorization'] = `Bearer ${bearerToken}`
 
-const getStrokes = async (url, storedData) => {
+const getPaginatedData = async (url, storedData = []) => {
   const { data } = await axios.get(url)
   storedData = [...storedData, data]
 
   if (url !== data._links.last.href) {
-    return await getStrokes(data._links.next.href, storedData)
+    return await getPaginatedData(data._links.next.href, storedData)
   }
 
   return storedData
@@ -22,12 +22,21 @@ const getStrokes = async (url, storedData) => {
 
 const handler = async () => {
   console.log('Getting all sessions')
-  const { data: sessionsData } = await axios.get(`${baseUrl}/activities`)
-  const sessions = sessionsData?.items
+  let sessions = await getPaginatedData(`${baseUrl}/activities`)
+  sessions = sessions.flatMap((session) => {
+    return session.items
+  })
+
   let filteredSessions = []
 
   for (const session of sessions) {
     console.log(`Looking at individual session data: ${session.id}`)
+
+    if (session.kind !== 'Practice') {
+      console.log('Session was a game. Stroke data not available.')
+      continue
+    }
+
     const filteredSession = {
       id: session.id,
       facility: session.facility.name,
@@ -39,11 +48,13 @@ const handler = async () => {
     }
 
     console.log('Getting all strokes from session')
-    const strokePages = await getStrokes(session._links.strokes.href, [])
+    const strokePages = await getPaginatedData(session._links.strokes.href)
 
     console.log('Assigning stroke data to session')
     for (const strokePage of strokePages) {
       strokePage.items.map((stroke) => {
+        if (!stroke.proBallMeasurement) return
+
         const filteredStroke = {
           id: stroke.id,
           proBallMeasurement: stroke.proBallMeasurement,
@@ -51,12 +62,18 @@ const handler = async () => {
           bay: stroke.bayName,
           createdAt: stroke.createdAt
         }
+        delete filteredStroke?.proBallMeasurement?.ballTrajectory
+        delete filteredStroke?.proBallMeasurement?.ballVelocity
         return filteredSession.strokes = [...filteredSession.strokes, filteredStroke]
       })
     }
 
+    filteredSession.totalStrokes = filteredSession.strokes.length
+
     console.log('Adding session to sessions array')
-    filteredSessions = [...filteredSessions, filteredSession]
+    if (filteredSession.totalStrokes > 0) {
+      filteredSessions = [...filteredSessions, filteredSession]
+    }
   }
 
   console.log('Writing session data to file in S3')
